f41b200b719a0ae22a0c01b0a7be4b0be57e4091
Aicha Mbongo <zindamoyen2@gmail.com>
2023-09-28 10:44:46 +0300

    created profiles and linked pages

--- user_app/urls.py
+++ user_app/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('profile_list/', views.profile_list, name='profile_list'),
-    path('login/', views.login_user, name='login')
-    # path('logout/', views.logout_user, name='logout'),
+    path('profile/<int:pk>', views.profile, name='profile'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Profile
 from django.contrib.auth import authenticate, login, logout
-
+from django.contrib.auth.forms import UserCreationForm
 def home(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render())
@@ -17,7 +17,42 @@ def profile_list(request):
     messages.success(request, "You Must be logged in to view this page...")
     return redirect('home')
    
+def profile (request, pk):
+   if request.user.is_authenticated:
+      profile = Profile.objects.get(user_id=pk)
+      return render(request, "profile.html", {"profile": profile})
+   else:
+    messages.success(request, "You Must be logged in to view this page...")
+    return redirect('home')
+
+      
+   
 def login_user(request):
-    return render(request, 'login.html', {}) 
+    if request.method == "POST":
+       username = request.POST['username']
+       password = request.POST['password']
+       user = authenticate(request, username=username, password=password)
+       if user is not None:
+          login(request, user)
+          messages.success(request, ("You have been logged in!"))
+          return redirect('home')
+       else:
+           messages.success(request, ("There was an error logging in, please try again"))
+           return redirect('login')
+          
+       
+    else:
+        return render(request, 'login.html', {}) 
+    
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out"))
+    return redirect('home')
+
+def register_user(request):
+   return render(request, 'authenticate/register_user.html', {})
+
+   
 
       
